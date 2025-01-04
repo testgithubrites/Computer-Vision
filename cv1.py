d56048611e7570d0d5c2c431680546c49199d3d7
Shruti <2821330.cse@pietgroup.co.in>
2025-01-04 16:29:25 +0530

    pushed

---
+++ cv1.py
@@ -0,0 +1,13 @@
+import cv2
+
+#Read images
+
+img = cv2.imread('lena.jpg' , 1) # 0(grayscale) , 1(colored) , -1(alpha channel)
+print(img)
+cv2.imshow('image' , img)
+k = cv2.waitKey()
+if k == 27:
+   cv2.destroyAllWindows()
+elif k == ord('s'):  #Save the image with tha name copied_image
+    cv2.imwrite('copied_image.png' , img)
+    cv2.destroyAllWindows()
